2fe9216c4356f25726cace21f6eb4c538de6a4f4
Akhil Verma <akhilverma@akhils-Mac.local>
2024-04-20 17:35:29 +0530

    Linear Search Leet Problems

---
+++ DSA/Searching/Linear_Search_Calculating_Maximum_Wealth.java
@@ -0,0 +1,31 @@
+package DSA.Searching;
+
+public class Linear_Search_Calculating_Maximum_Wealth {
+    public static void main(String[] args) {
+        
+    int[][] accounts = {
+                        {1,2,3},
+                        {3,2,4},
+                        {3,4,3},
+                        {3,2,13}
+                    };
+
+    maxWeath(accounts);
+}
+
+    static void maxWeath(int[][] accounts){
+        int ans = Integer.MIN_VALUE;
+        for (int person = 0; person < accounts.length; person++) {
+            // For every column
+            int rowSum = 0;
+            for (int account = 0; account < accounts[person].length; account++) {
+                rowSum += accounts[person][account];
+            }
+            System.out.println("Total Wealth of customer " + (person + 1) + ": " + rowSum);
+            if(rowSum > ans){
+                ans = rowSum;
+            }
+        }
+        System.out.println("Max is : " + ans);
+    }
+}
--- DSA/Searching/Linear_Search_Finding_Even_Digits.java
+++ DSA/Searching/Linear_Search_Finding_Even_Digits.java
@@ -1,7 +1,48 @@
 package DSA.Searching;
-
 public class Linear_Search_Finding_Even_Digits {
     public static void main(String[] args) {
-    
+        int[] nums = {12, 345, 2, 6, 7896};
+        findEvenNumberDigits(nums);
+        // findEvenNumberDigits2(nums);
+
     }
+
+    static void findEvenNumberDigits(int[] nums) {
+        for (int index = 0; index < nums.length; index++) {
+            int num = nums[index];
+            int digitCount = countDigits(num);
+
+            if (digitCount % 2 == 0) {
+                System.out.println(num + " has an even number of digits.");
+            }
+        }
+    }
+
+    static int countDigits(int num) {
+        if (num == 0) return 1; // Special case for num = 0
+        int count = 0;
+        while (num != 0) {
+            num /= 10;
+            count++;
+        }
+        return count;
+    }
+
+
+     // Simplest way
+    // static void findEvenNumberDigits2(int[] nums){
+    //     for (int index = 0; index < nums.length; index++) {
+    //         // System.out.println(nums[index]);
+    //         String numStr = String.valueOf(nums[index]);
+    //         // System.out.println(numStr.length());
+
+    //         if(numStr.length() % 2 == 0){
+    //             System.out.println(numStr);
+    //         }
+    //     }
+    // }
+
 }
+
+
+  
