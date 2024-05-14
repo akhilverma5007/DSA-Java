21e438b571abcb3a7127fca314016f93c5560c9f
Akhil Verma <akhilverma@Akhils-MacBook-Pro.local>
2024-05-14 16:32:58 +0530

    Patterns

--- Fundamentals/Patterns_Using_Java.java
+++ Fundamentals/Patterns_Using_Java.java
@@ -3,15 +3,38 @@ package Fundamentals;
 public class Patterns_Using_Java {
     public static void main(String[] args) {
         pattern1(5);
+        pattern2(5);
+        pattern3(3);
     }
 
     static void pattern1(int n){
-        for (int row = 0; row < n; row++) {
-        //    System.out.println("*");
-           for(int col = 1; col <= row; col++){
-            System.out.print("* ");
-           }
-           System.out.println();
+        for(int row = 0; row < n; row++){
+            for(int col = 0; col < row; col++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
+        System.out.println();
+    }
+
+
+    static void pattern2(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    static void pattern3(int n){
+        for(int i = 0; i < n; i++){
+            for (int j = 0; j <= n-i+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
